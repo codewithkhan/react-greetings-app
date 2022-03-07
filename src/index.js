678469f637bf34f80cee40d74b61da9e7205bed8
Md shadab khan <66864511+codewithkhan@users.noreply.github.com>
2022-03-07 10:06:28 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+
+var greetings = "";
+var name = prompt("Please Enter your Name");
+var cssStyle = {};
+var currentTime = new Date().getHours();
+
+if (currentTime >= 1 && currentTime <= 11) {
+  greetings = "Good Morning!";
+  cssStyle.color = "green";
+} else if (currentTime >= 19 && currentTime <= 23) {
+  greetings = "Good Night!";
+  cssStyle.color = "aqua";
+} else if (currentTime >= 12 && currentTime <= 18) {
+  greetings = "Good AfterNoon!";
+  cssStyle.color = "yellow";
+}
+
+ReactDOM.render(
+  <>
+    <div className="container">
+      <a href="./index.html" target="_khan">
+        <img
+          style={{ padding: "50px", marginTop: "25px" }}
+          src="https://picsum.photos/500/500?grayscale"
+          alt="Error"
+        />
+      </a>
+      <p>
+        {`Hello ${name}`} <span style={cssStyle}>{greetings}</span>
+      </p>
+    </div>
+  </>,
+  document.getElementById("root")
+);
